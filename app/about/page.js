08af3ce6989300a5c5c9787ee0048c9cd412a622
shahriar <amivaloasi>
2023-08-24 01:14:55 +0600

    about and contact page added

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,28 @@
 import React from "react";
+import Container from "@/components/container";
 
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <Container>
+      <div className="space-y-5">
+        <h2 className="text-2xl font-semibold text-center">About US</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+          eveniet repellat. In numquam, error molestias eius fuga accusantium
+          distinctio blanditiis sapiente repellat ut, voluptatibus modi minus
+          eaque? Impedit reprehenderit vero hic. Amet corrupti quos harum unde
+          sint, dolor quibusdam voluptate eius nulla inventore praesentium
+          deserunt odio deleniti esse exercitationem obcaecati.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga,
+          eveniet repellat. In numquam, error molestias eius fuga accusantium
+          distinctio blanditiis sapiente repellat ut, voluptatibus modi minus
+          eaque? Impedit reprehenderit vero hic. Amet corrupti quos harum unde
+          sint, dolor quibusdam voluptate eius nulla inventore praesentium
+          deserunt odio deleniti esse exercitationem obcaecati.
+        </p>
+      </div>
+    </Container>
+  );
 }
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,3 +1,32 @@
+import ContactForm from "@/components/ContactForm";
+import Container from "@/components/container";
+import {
+  LocationMarkerIcon,
+  MailIcon,
+  PhoneIcon,
+} from "@heroicons/react/outline";
 export default function Contact() {
-  return <div>page</div>;
+  return (
+    <Container>
+      <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
+        Contact
+      </h1>
+      <div className="text-center">
+        <p className="text-lg">We are a here to help.</p>
+      </div>
+
+      <div className="grid my-10 md:grid-cols-2">
+        <div className="my-10">
+          <h2 className="text-2xl font-semibold dark:text-white">Ostad</h2>
+          <p className="max-w-sm mt-5">
+            Have something to say? We are here to help. Fill up the form or send
+            email or call phone.
+          </p>
+        </div>
+        <div>
+          <ContactForm />
+        </div>
+      </div>
+    </Container>
+  );
 }
